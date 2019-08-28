f0febe4d0ad36fdc2aa135fcefbcd50f2145fc6a
mntmn <lukas@mntmn.com>
2019-08-28 18:15:08 +0200

    modify example buildscript paths

--- examples/conway/build-conway.sh
+++ examples/conway/build-conway.sh
@@ -1,17 +1,17 @@
-COMPILE="arm-none-eabi-gcc -std=gnu99 -nostdlib -O2 -c -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard -I../lib -I../include"
-LINK="arm-none-eabi-gcc -T ../link.ld -std=gnu99 -nostdlib -O2 -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard"
+COMPILE="arm-none-eabi-gcc -std=gnu99 -nostdlib -O2 -c -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard -I../../lib -I../../include"
+LINK="arm-none-eabi-gcc -T ../../link.ld -std=gnu99 -nostdlib -O2 -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard"
 NAME=conway
 
 mkdir -p build
 
-cppcheck --template='{file}:{line}:{severity}:{message}' --enable=all -I. -I../lib -I../include $NAME.c
+cppcheck --template='{file}:{line}:{severity}:{message}' --enable=all -I. -I../../lib -I../../include $NAME.c
 
-$COMPILE -I. -o build/$NAME.o $NAME.c
-$COMPILE -o build/idiv.o ../lib/div/idiv.S
-$COMPILE -o build/idivmod.o ../lib/div/idivmod.S
-$COMPILE -o build/ldivmod.o ../lib/div/ldivmod.S
-$COMPILE -o build/memcpy.o ../lib/memory/memcpy.c
-$COMPILE -o build/printf.o ../lib/printf/printf.c
+$COMPILE -o build/$NAME.o $NAME.c
+$COMPILE -o build/idiv.o ../../lib/div/idiv.S
+$COMPILE -o build/idivmod.o ../../lib/div/idivmod.S
+$COMPILE -o build/ldivmod.o ../../lib/div/ldivmod.S
+$COMPILE -o build/memcpy.o ../../lib/memory/memcpy.c
+$COMPILE -o build/printf.o ../../lib/printf/printf.c
 
 $LINK -o $NAME build/conway.o build/memcpy.o build/printf.o build/*div*.o -L. 
 arm-none-eabi-objcopy -O binary $NAME $NAME.bin
--- examples/shell/build-shell.sh
+++ examples/shell/build-shell.sh
@@ -1,5 +1,5 @@
-COMPILE="arm-none-eabi-gcc -std=gnu99 -nostdlib -O2 -c -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard -I../lib -I../include"
-LINK="arm-none-eabi-gcc -T ../link.ld -std=gnu99 -nostdlib -O2 -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard"
+COMPILE="arm-none-eabi-gcc -std=gnu99 -nostdlib -O2 -c -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard -I../../lib -I../../include"
+LINK="arm-none-eabi-gcc -T ../../link.ld -std=gnu99 -nostdlib -O2 -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=hard"
 NAME=shell
 
 cppcheck --template='{file}:{line}:{severity}:{message}' --enable=all -I. $NAME.c
@@ -7,12 +7,12 @@ cppcheck --template='{file}:{line}:{severity}:{message}' --enable=all -I. $NAME.
 mkdir -p build
 
 $COMPILE -I. -o build/$NAME.o $NAME.c
-$COMPILE -o build/idiv.o ../lib/div/idiv.S
-$COMPILE -o build/idivmod.o ../lib/div/idivmod.S
-$COMPILE -o build/ldivmod.o ../lib/div/ldivmod.S
-$COMPILE -o build/memset.o ../lib/memory/memset.c
-$COMPILE -o build/memcpy.o ../lib/memory/memcpy.c
-$COMPILE -o build/printf.o ../lib/printf/printf.c
+$COMPILE -o build/idiv.o ../../lib/div/idiv.S
+$COMPILE -o build/idivmod.o ../../lib/div/idivmod.S
+$COMPILE -o build/ldivmod.o ../../lib/div/ldivmod.S
+$COMPILE -o build/memset.o ../../lib/memory/memset.c
+$COMPILE -o build/memcpy.o ../../lib/memory/memcpy.c
+$COMPILE -o build/printf.o ../../lib/printf/printf.c
 
 $LINK -o $NAME build/$NAME.o build/memcpy.o build/memset.o build/printf.o build/*div*.o -L. -lm
 arm-none-eabi-objcopy -O binary $NAME $NAME.bin
