be4fc62e30b542e5d92f5de9924c28a1520a7124
mntmn <lukas@mntmn.com>
2019-08-28 17:52:51 +0200

    add cppcheck to zz9k build.sh

--- zz9k-loader/build.sh
+++ zz9k-loader/build.sh
@@ -4,6 +4,8 @@ export INCLUDE2=$HOME/code/vbcc/targets/m68k-amigaos/include2
 export PATH=$PATH:$VBCC/bin
 export NAME=zz9k
 
+cppcheck --template='{file}:{line}:{severity}:{message}' --enable=all -I. $NAME.c
+
 vc +aos68k -I$INCLUDE -I$INCLUDE2 -I. -c99 -O2 -o $NAME $NAME.c -ldebug -lamiga -lauto
 
 rm $NAME.lha
