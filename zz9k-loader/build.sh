be885235d282d9bf53c64f8893800b4b8aa516fe
mntmn <lukas@mntmn.com>
2019-08-28 16:44:01 +0200

    add 'zz9k' loader/runner tool

---
+++ zz9k-loader/build.sh
@@ -0,0 +1,10 @@
+export VBCC=$HOME/code/vbcc
+export INCLUDE=$HOME/code/vbcc/targets/m68k-amigaos/include
+export INCLUDE2=$HOME/code/vbcc/targets/m68k-amigaos/include2
+export PATH=$PATH:$VBCC/bin
+export NAME=zz9k
+
+vc +aos68k -I$INCLUDE -I$INCLUDE2 -I. -c99 -O2 -o $NAME $NAME.c -ldebug -lamiga -lauto
+
+rm $NAME.lha
+lha a0 $NAME.lha $NAME
